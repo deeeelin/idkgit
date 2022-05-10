dd1716bbf74c0a76cefb66763f118496fc934153
Derek <dereklin100503@gmail.com>
2022-05-10 16:15:44 +0800

    commit on 2022年 5月10日 週二 15時56分30秒 CST

--- gitclone.bash
+++ gitclone.bash
@@ -3,16 +3,18 @@
 read -p "clone where? " url
 read -p "create dir? yes or no? "
  
-echo "start cloning......"
+
 
 if [[ $REPLY == "yes" ]];then
     read -p "what name? " name
     read -p "under where? " fdir
     cd $fdir
     mkdir ${name}
+    echo "start cloning......"
     git clone $url "$fdir/$name/"
 else 
     read -p "save where" fdir
+    echo "start cloning......"
     git clone "$url" "$fdir"
 
 fi
