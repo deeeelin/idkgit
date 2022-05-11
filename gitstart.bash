173aea2b8b5312ed74467e9f114d94caff5784c8
Derek <dereklin100503@gmail.com>
2022-05-11 19:31:33 +0800

    commit on 2022年 5月11日 週三 19時31分33秒 CST

--- gitstart.bash
+++ gitstart.bash
@@ -58,7 +58,7 @@ function do_pull () {
             chmod +x gpul_$i.bash 
             ./gpul_$i.bash
             cond=$?
-            echo "$cond"
+            echo "this is fucking $cond"
             if [[ $cond -eq 87 ]];then
                 echo "$i pull failed" 
                 echo "no previous reference for auto"
